4878066e970ba7092245bfcd6ae54f2e9ee35c4e
empitsu0813 <oneok0813@gmail.com>
2020-03-09 09:42:11 +0900

    Changes

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,11 +2,13 @@
   <div class="main">
     <div class="sidebar">
       <div class="categoryList">
+        <button @click="createCategory()">Create Category</button>
         <div :key="category.id" v-for="category in categories">
           <div class="categoryName" @click="openCategory(category)">{{ category.name  }}</div>
         </div>
       </div>
       <div class="noteList" v-if="selectedCategory">
+        <button @click="createNote()">Create Note</button>
         <div :key="note.id" v-for="note in selectedCategory.notes">
           <div class="noteTitle" @click="openNote(note)">{{ note.title }}</div>
         </div>
@@ -45,6 +47,19 @@ export default class Main extends Vue {
 
   selectedNote: Note | null = null;
 
+  // todo: idは category_id_{uuid} にする
+  defaultCategory: Category = {
+    id: 'category_id',
+    name: 'category name',
+    notes: [],
+  }
+
+  defaultNote: Note = {
+    id: 'note_id',
+    title: 'category title',
+    body: 'aiueo',
+  }
+
   openCategory(category: Category): void {
     if (this.selectedCategory === null || this.selectedCategory?.id !== category.id) {
       this.selectedCategory = category;
@@ -56,5 +71,23 @@ export default class Main extends Vue {
       this.selectedNote = note;
     }
   }
+
+  createCategory(): void {
+    this.categories.push(this.defaultCategory);
+  }
+
+  createNote(): void {
+    if (this.selectedCategory) {
+      this.selectedCategory.notes.push(this.defaultNote);
+    }
+  }
+
+  // deleteCategory(category: Category): void {}
+
+  // deleteNote(note: Note): void {}
+
+  // updateCategory(category: Category): void {}
+
+  // updateNote(note: Note): void {}
 }
 </script>
